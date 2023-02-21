86f8fb835ce0b6badc857a9fc9c6ef167cd89bc7
ISXOD <Nikkrass2003@yandex.ru>
2023-02-21 10:38:30 +0300

    Change screen size

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,56 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height);
+
 int main(void)
 {
-    GLFWwindow* window;
+    GLFWwindow* pWindow;
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "glfwInit failed" << std::endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle City", nullptr, nullptr);
+    if (!pWindow)
     {
+        std::cout << "glfwCreateWindow failed" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(pWindow);
 
-    if (!gladLoadGL()) {
+    if (!gladLoadGL()) 
+    {
         std::cout << "Can't load GLAD!" << std::endl;
         return -1;
     }
 
-    std::cout << "OpenGL" << GLVersion.major << "." << GLVersion.minor << std::endl;
 
     glClearColor(0, 1, 0, 1);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
@@ -45,4 +59,12 @@ int main(void)
 
     glfwTerminate();
     return 0;
+}
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height) 
+{
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+
 }
